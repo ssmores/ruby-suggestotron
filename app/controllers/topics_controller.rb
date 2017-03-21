f577462c0da23dcea8fa6f110a10537ac8aba06a
Stefanie <ssmores@hotmail.com>
2017-03-21 17:30:56 +0000

    Commented out downvote options, which was removed entirely from the index.html file.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -67,6 +67,12 @@ class TopicsController < ApplicationController
     redirect_to(topics_path)
   end
 
+  # def downvote
+  #   @topic = Topic.find(params[:id])
+  #   @topic.votes.count = @topic.votes.count.to_i - 1
+  #   redirect_to(topics_path)
+  # end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
